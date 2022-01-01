25f29736a1324a470368aa1a1e443d33ae3aa30e
GamerKing1403 <55126852+GamerKing1403@users.noreply.github.com>
2022-01-01 19:09:49 +0530

    Delete Assignment 3 directory

--- Assignment 3/WAP to print all alphabets from a to z.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    for(int a=97;a<=122;a++){
-        printf("%c ",(char)a);
-    }
-return 0;
-}
--- Assignment 3/WAP to printf all characters with their ASCII values.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    for(int i=65;i<=126;i++){
-        printf("The character is %c with ASCII value %d\n",(char)i,i);
-    }
-    return 0;
-}
